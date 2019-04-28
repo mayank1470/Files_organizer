68b59ace2d107caab36f9ed7c55ab49183ce80b3
Mayank Soni <sonimayank110@ymail.com>
2019-04-28 23:22:17 +0530

    Fixed db and py issues
    Scripts now ignore db and py files from Relocation process

--- database_utils.py
+++ database_utils.py
@@ -9,15 +9,22 @@ def create_tables(path):
     connection,cursor=loadConnection(path)
     query='create table if not exists extentions_processed (extention text);'
     cursor.execute(query)
+    connection.commit()
 def saveExtentions(extentions,path):
     connection,cursor=loadConnection(path)
     cursor.executemany('insert into extentions_processed (extention) values(?)',[(x,) for x in extentions])
     connection.commit()
 def fetchExtentions(path):
     connection,cursor=loadConnection(path)
-    extentions_processed=list(cursor.fetchall())
+    cursor.execute('SELECT * FROM extentions_processed')
+    extentions=cursor.fetchall()
+    final_extentions=[]
+    for extention in extentions:
+        #TODO
+        temp,=extention
+        final_extentions.append(temp)
     clearExtentions(cursor,connection)
-    return extentions_processed
+    return final_extentions
 def clearExtentions(cursor,connection):
     cursor.execute('delete from extentions_processed')
     connection.commit()
--- main.py
+++ main.py
@@ -18,6 +18,8 @@ else:
     extentions=[]
 for (dirpath,dirnames,files) in os.walk(path):
     for filename in files:
+        if(filename=='extentions.db'):
+            continue
         extpos=filename.find('.')
         extention=filename[extpos+1:]
         print(dirpath)
@@ -25,18 +27,24 @@ for (dirpath,dirnames,files) in os.walk(path):
         print(filename)
         print(extention)
         if(extention in extentions):
+            if(extention =='py'):
+                #Do not move Python Files
+                continue
             #No need to make a new folder
             sourcePath=dirpath+delimit+filename
             destinationPath=dirpath+delimit+extention+delimit+filename
-            print('Source Path ',sourcePath)
-            print('Destination Path',destinationPath)
+            # print('Source Path ',sourcePath)
+            # print('Destination Path',destinationPath)
             shutil.move(sourcePath,destinationPath)
         else:
             sourcePath=dirpath+delimit+filename
             destinationPath=dirpath+delimit+extention+delimit+filename
-            print('Source Path ',sourcePath)
-            print('Destination Path',destinationPath)
+            # print('Source Path ',sourcePath)
+            # print('Destination Path',destinationPath)
             extentions.append(extention)
+            os.mkdir(dirpath+delimit+extention)
             shutil.move(sourcePath,destinationPath)
+    #Break the loop for 1 Iteration only and organize only level 1
+    break
 print(extentions)
 saveExtentions(extentions,path)
